dab1182260ae5a87f8266c8a6a76fb721fc7c063
Artemy-jzs-161 <60886539+Artemy-jzs-161@users.noreply.github.com>
2024-03-06 22:12:54 +0700

    Update Main.java

--- src/main/java/ru/artemborovik/Main.java
+++ src/main/java/ru/artemborovik/Main.java
@@ -2,6 +2,6 @@ package ru.artemborovik;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!!!!!!!!");
     }
 }

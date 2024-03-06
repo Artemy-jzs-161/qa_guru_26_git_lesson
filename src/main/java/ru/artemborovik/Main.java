9a036646a7a7df3cdb0d337348a1dacc72258a26
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-03-06 22:20:59 +0700

    commit in new_branch

--- src/main/java/ru/artemborovik/Main.java
+++ src/main/java/ru/artemborovik/Main.java
@@ -3,5 +3,7 @@ package ru.artemborovik;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello QA.GURU!");
+
     }
 }
